9e5f3e74fc906c07c54ce129613aed015481ec89
Akaash J <akaashjanarthanan@gmail.com>
2025-01-13 15:20:18 +0530

    Create Binary Tree Inorder Traversal.c

---
+++ Binary Tree Inorder Traversal.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void inorderHelper(struct TreeNode* root, int *result, int *index){
+    if(root == NULL) return;
+    inorderHelper(root->left,result,index);
+    result[(*index)++] = root->val;
+    inorderHelper(root->right, result, index);
+}
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    if(root==NULL){
+        *returnSize = 0;
+        return NULL;
+    }
+
+    int *result = (int*)malloc(100*sizeof(int));
+    int index = 0;
+
+    inorderHelper(root, result, &index);
+    *returnSize = index;
+    return result;
+}
