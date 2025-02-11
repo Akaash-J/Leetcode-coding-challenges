cb1e6d26ef8264b09a2bf592e74d271c6cc318c8
Akaash J <akaashjanarthanan@gmail.com>
2025-02-11 08:59:20 +0530

    Create Linked List Cycle.c

---
+++ Linked List Cycle.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    if(head==NULL) return 0;
+    struct ListNode* slow = head, *fast = head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast){
+            return 1;
+        }
+    }
+    return 0;
+}
